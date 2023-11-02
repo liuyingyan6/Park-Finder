a667221451bdd95d1a18693a4a90280c126b06ca
C23018219 <WangT63@cardiff.ac.uk>
2023-11-02 13:42:54 +0000

    FIX

---
+++ team1/src/main/java/com/team1/team1/hackathon/Park.java
@@ -0,0 +1,39 @@
+package com.team1.team1.hackathon;
+
+import java.util.List;
+
+public class Park { // Define a park class for the park information
+        private String name;
+        private String type;
+        private int distance;
+        private List<String> amenities;
+        private List<String> accessibility;
+
+        public Park(String name, String type, int distance, List<String> amenities, List<String> accessibility, List<String> comments) {
+            this.name = name;
+            this.type = type;
+            this.distance = distance;
+            this.amenities = amenities;
+            this.accessibility = accessibility;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getType() {
+            return type;
+        }
+
+        public int getDistance() {
+            return distance;
+        }
+
+        public List<String> getAmenities() {
+            return amenities;
+        }
+
+        public List<String> getAccessibility() {
+            return accessibility;
+        }
+}
