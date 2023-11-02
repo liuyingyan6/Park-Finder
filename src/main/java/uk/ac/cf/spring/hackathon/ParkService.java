5df1cecc6fe8a1186b669a5e8c603f38afed12cf
c2060022 <lalsiel1@cardiff.ac.uk>
2023-11-02 13:01:39 +0000

    Controller and classes for filtering parks

--- src/main/java/uk/ac/cf/spring/hackathon/ParkService.java
+++ src/main/java/uk/ac/cf/spring/hackathon/ParkService.java
@@ -1,2 +1,19 @@
-package uk.ac.cf.spring.hackathon;public class ParkService {
+package uk.ac.cf.spring.hackathon;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+@Service
+public class ParkService {
+        private List<Park> parks = new ArrayList<>(Arrays.asList(  // List of parks with sample data for testing functionality
+                new Park("Park A", "Urban Park", 5, Arrays.asList("Cafe", "Toilets"), Arrays.asList("Wheelchair Access"), new ArrayList<>()),
+                new Park("Park B", "Sports Park", 10, Arrays.asList("Sports Facilities", "Cafe"), Arrays.asList("Car Park"), new ArrayList<>()),
+                new Park("park C", "Dog Park", 15, Arrays.asList("Children's area", "cafe"),Arrays.asList("Animals Allowed"), new ArrayList<>())
+        ));
+
+        public List<Park> getAllParks() {  // Retrieve all parks
+            return parks;
+        }
 }
