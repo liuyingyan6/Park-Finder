5febb7e0d6aac466da73e8ff471815bcbbc2efad
Jinghao Gong <GongJ13@cardiff.ac.uk>
2023-11-02 15:09:19 +0000

    add footer.html

--- team1/src/main/java/com/team1/team1/park/ParkController.java
+++ team1/src/main/java/com/team1/team1/park/ParkController.java
@@ -1,15 +1,24 @@
 package com.team1.team1.park;
 
+import com.team1.team1.hackathon.ParkFilterController;
+import com.team1.team1.hackathon.ParkService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 public class ParkController {
+    private static final Logger log = LoggerFactory.getLogger(ParkFilterController.class);
+
     // In practice, this should be the logic for getting park data from a database or other data source
     private List<Park> parks = List.of(
             new Park("Park1", "park1.jpg", "city"),
@@ -26,7 +35,7 @@ public class ParkController {
             // 更多公园数据
     );
 
-    @GetMapping("/parks")
+//    @GetMapping("/parks")
     public String showParks(
             @RequestParam(required = false) String searchText,
             @RequestParam(required = false) String parkType,
@@ -67,5 +76,37 @@ public class ParkController {
         }
 
     }
+    @Autowired
+    private ParkService parkService;
+
+    @RequestMapping("/parks")
+    public String viewParks(Model model,
+                            @RequestParam(required = false) List<String> parkType,
+                            @RequestParam(required = false) Integer distance,
+                            @RequestParam(required = false) List<String> amenities,
+                            @RequestParam(required = false) List<String> accessibility) {
+
+        log.info("Received park types: {}", parkType);
+        log.info("Received distance: {}", distance);
+        log.info("Received amenities: {}", amenities);
+        log.info("Received accessibility: {}", accessibility);
+
+        // Get all parks from the ParkService
+        List<com.team1.team1.hackathon.Park> allParks = parkService.getAllParks();
+        log.info("Number of parks before filtering: {}", allParks.size());
+        // Filter the parks based on the search and other criteria
+        List<com.team1.team1.hackathon.Park> filteredParks = allParks.stream()
+                .filter(park -> parkType == null || parkType.isEmpty() || parkType.stream().anyMatch(type -> type.equalsIgnoreCase(park.getType())))
+                .filter(park -> distance == null || park.getDistance() <= distance)
+                .filter(park -> amenities == null || amenities.isEmpty() || amenities.stream().allMatch(amenity -> park.getAmenities().stream().anyMatch(parkAmenity -> parkAmenity.equalsIgnoreCase(amenity))))
+                .filter(park -> accessibility == null || accessibility.isEmpty() || accessibility.stream().allMatch(access -> park.getAccessibility().stream().anyMatch(parkAccess -> parkAccess.equalsIgnoreCase(access))))
+                .collect(Collectors.toList());
+
+        log.info("Number of parks after filtering: {}", filteredParks.size());
+        model.addAttribute("parks", filteredParks);
+        model.addAttribute("slideImgs", slideImg);
+        return "park-recommendation"; // Assuming "filter" is the name of the template
+    }
+
 }
 
